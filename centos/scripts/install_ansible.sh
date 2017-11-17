854cb4f80d08c5588cd6d12ae1358fdd878fb416
Sebastiaan Vreeswijk <s.vreeswijk@gmail.com>
2017-11-17 03:23:45 +0100

    dit zou een complete install van openshift met 1 master en 1 node moeten zijn

--- centos/scripts/install_ansible.sh
+++ centos/scripts/install_ansible.sh
@@ -1,3 +1,5 @@
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
 yum -y --enablerepo=epel install ansible pyOpenSSL
+cd ~
+git clone https://github.com/openshift/openshift-ansible.git
