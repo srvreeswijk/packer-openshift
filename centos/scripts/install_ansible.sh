d6de65a56ae26c0098a0003a6125660455d49757
Sebastiaan Vreeswijk <svreeswijk@Server12604.i3d.net>
2017-11-20 13:45:39 +0100

    first try

--- centos/scripts/install_ansible.sh
+++ centos/scripts/install_ansible.sh
@@ -3,9 +3,8 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 yum -y --enablerepo=epel install ansible pyOpenSSL
 yum install -y psacct glusterfs-fuse docker-1.12.6 git bind-utils bridge-utils bash-completion kexec-tools sos
 cd /home/vagrant/
-
-date > /etc/vagrant_provisioned_at
 git clone https://github.com/openshift/openshift-ansible.git /home/vagrant/openshift-ansible
+git clone https://github.com/srvreeswijk/packer-openshift /home/vagrant/packer-openshift
 chown vagrant /home/vagrant/openshift-ansible
 sudo mv /etc/ansible/hosts /etc/ansible/hosts.bak
-sudo cp /vagrant/ansible-hosts.cfg /etc/ansible/hosts
+sudo cp /home/vagrant/packer-openshift/centos/vagrant/ansible-hosts.cfg /etc/ansible/hosts
